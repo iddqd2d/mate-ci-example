204fcb71e6579c9f04b800794f64b9295404674a
boroda4436 <chupika.bohdan@ukr.net>
2019-06-12 19:34:05 +0300

    added src

---
+++ src/main/java/com/mate/springdemoci/SpringDemoCiApplication.java
@@ -0,0 +1,13 @@
+package com.mate.springdemoci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mate/springdemoci/SpringDemoCiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mate.springdemoci;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDemoCiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
